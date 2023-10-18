e237d65f96486793d34a905aa5dac7656e5e3076
terrencestella <terrence.s.stella@gmail.com>
2023-10-18 21:28:26 +0200

    made final changes to terstel package

--- src/terstel/main.py
+++ src/terstel/main.py
@@ -1,10 +1,10 @@
-import argparse
+from argparse import ArgumentParser
 import os
 import functions as f
 
 #to get the current working directory
 dir = os.getcwd()
-parser = argparse.ArgumentParser(description='Application should receive three arguments, \
+parser = ArgumentParser(description='Application should receive three arguments, \
 the paths to each of the dataset files and also the countries to filter as \
 the client wants to reuse the code for other countries.')
 parser.add_argument('clients_file',type=str,help='The file name (including extension) containing the client data')
@@ -24,9 +24,9 @@ def main(client_file, financial_file, countries):
     client_df = f.drop_column(client_df,'email')
     financial_df = f.load_data(spark, f'{dir}\data\{financial_file}')
     financial_df = f.drop_column(financial_df,'cc_n')
-    filtered_client_df = f.filter_data(client_df, countries)
+    filtered_client_df = f.filter_country(client_df, countries)
     joined_df = f.join_dfs(filtered_client_df,financial_df,'id','inner')
-    df = f.rename_columns(joined_df,renaming_columns)
+    df = f.rename_column(joined_df,renaming_columns)
     df.write.csv("client_data\output.csv", header=True)
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 import unittest
 from chispa.dataframe_comparer import assert_df_equality
 from pyspark.sql import SparkSession
-from src.main import load_data, filter_data, rename_columns  # adjust import path accordingly
+import src.terstel.functions as fn
 
 class MainTest(unittest.TestCase):
     @classmethod
@@ -17,23 +17,23 @@ class MainTest(unittest.TestCase):
 
     def test_load_data(self):
         path = "path_to_test_data/test_clients.csv"
-        df = load_data(self.spark, path)
+        df = fn.load_data(self.spark, path)
         # Verify the schema, number of rows, or other properties of df
 
     def test_filter_data(self):
         # Load test data
         path = "path_to_test_data/test_clients.csv"
-        df = load_data(self.spark, path)
+        df = fn.load_data(self.spark, path)
         # Filter data
-        filtered_df = filter_data(df, ["United Kingdom", "Netherlands"])
+        filtered_df = fn.filter_data(df, ["United Kingdom", "Netherlands"])
         # Verify the result, e.g., number of rows, values in the 'country' column, etc.
 
     def test_rename_columns(self):
         # Load test data
         path = "path_to_test_data/test_financial.csv"
-        df = load_data(self.spark, path)
+        df = fn.load_data(self.spark, path)
         # Rename columns
-        renamed_df = rename_columns(df)
+        renamed_df = fn.rename_columns(df)
         # Verify the result, e.g., column names
 
 if __name__ == '__main__':
